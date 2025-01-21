95bdfe823e59bd0991bdf09813073f05dc120e86
Emilia Repo <emiliarepo@icloud.com>
2025-01-21 21:15:34 +0200

    Initial commit

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				beige: "var(--color-beige)",
+				primary: "var(--color-primary)",
+				"primary-active": "var(--color-primary-active)",
+				gray: "var(--color-gray)",
+				light: "var(--color-light)",
+			},
+			fontFamily: {
+				serif: ["Gelasio Variable", "serif"],
+				display: ["Platypi Variable", "serif"],
+			},
+		},
 	},
 	plugins: [],
-}
+	darkMode: "class", // Use class-based dark mode
+};
